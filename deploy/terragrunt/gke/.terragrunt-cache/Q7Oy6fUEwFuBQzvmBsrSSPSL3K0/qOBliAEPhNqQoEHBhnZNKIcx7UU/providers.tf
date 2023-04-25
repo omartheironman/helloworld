a3e1099c80d8a3d8ca4f6c59cd7423d0d7345527
Omar Moharrem <omar.moharrem@unity3d.com>
2023-04-25 14:45:55 -0400

    fix files

--- deploy/terragrunt/gke/.terragrunt-cache/Q7Oy6fUEwFuBQzvmBsrSSPSL3K0/qOBliAEPhNqQoEHBhnZNKIcx7UU/providers.tf
+++ deploy/terragrunt/gke/.terragrunt-cache/Q7Oy6fUEwFuBQzvmBsrSSPSL3K0/qOBliAEPhNqQoEHBhnZNKIcx7UU/providers.tf
@@ -14,7 +14,6 @@ data "google_client_config" "default" {}
 
 provider "google" {
   # Configuration options
-  credentials = "${file("/Users/omar.moharrem/.config/gcloud/application_default_credentials.json")}" #Injected into env
   project = var.project
   region  = var.region
 }
